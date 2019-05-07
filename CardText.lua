21ad56fa396de58be6937e3bf0c069a8b2be77fd
Lucas Pinheiro <lucas_p@outlook.com>
2019-05-07 00:14:45 -0300

    Show large text for card stats

---
+++ CardText.lua
@@ -0,0 +1,71 @@
+local Class = require("Utils.Class")
+
+local attributes = {
+    [1] = "EARTH",
+    [2] = "WATER",
+    [4] = "FIRE",
+    [8] = "WIND",
+    [16] = "LIGHT",
+    [32] = "DARK",
+    [64] = "???"
+}
+local races = {
+    [1] = "Warrior",
+    [2] = "Spellc",
+    [4] = "Fairy",
+    [8] = "Fiend",
+    [16] = "Zombie",
+    [32] = "Machine",
+    [64] = "Aqua",
+    [128] = "Pyro",
+    [256] = "Rock",
+    [512] = "Winged",
+    [1024] = "Plant",
+    [2048] = "Insect",
+    [4096] = "Thunder",
+    [8192] = "Dragon",
+    [16384] = "Beast",
+    [32768] = "Bst-W.",
+    [65536] = "Dino.",
+    [131072] = "Fish",
+    [262144] = "Sea S.",
+    [524288] = "Reptile",
+    [1048576] = "Psychic",
+    [2097152] = "Divine",
+    [4194304] = "???",
+    [8388608] = "Wyrm",
+    [16777216] = "Cyberse"
+}
+
+local CardText =
+    Class.new(
+    {
+        width = 225,
+        height = 72
+    },
+    function(self, model)
+        self.displayObject = display.newGroup()
+        self.displayObject.x = 0
+        self.displayObject.y = 132
+
+        display.newRect(self.displayObject, 0, 0, 225, 72)
+
+        local attribute = attributes[model:getAttribute()] or "???"
+        local race = races[model:getRace()] or "???"
+        local text =
+            display.newText(
+            {
+                parent = self.displayObject,
+                text = string.format("%s / %s\n%4d / %4d", race, attribute, model:getAttack(), model:getDefense()),
+                width = self.width,
+                fontSize = 27,
+                align = "center"
+            }
+        )
+        text:setFillColor(0, 0, 0)
+
+        print(model:getName(), model:getRace())
+    end
+)
+
+return CardText
--- CardView.lua
+++ CardView.lua
@@ -2,6 +2,7 @@ local Class = require("Utils.Class")
 local Sound = require("Sound")
 local DisplayGroups = require("DisplayGroups")
 local Transition = require("Transition")
+local CardText = require("CardText")
 local Event = require("EventSystem.Event")
 
 local scale = 1.5
@@ -21,7 +22,7 @@ local CardView =
 function CardView:moveToHand(position, wait)
     local finalX = self:_getXAtPosition(position)
     local finalY = self:_getYAtPosition(self.side, position)
-    self.materialNumber.displayGroup.x = finalX - self.displayObject.width / 2 + 22
+    self.materialNumber.displayGroup.x = finalX - self.image.width / 2 + 22
     Transition.to(
         self.displayObject,
         {
@@ -37,8 +38,13 @@ function CardView:moveToHand(position, wait)
 end
 
 function CardView:createDisplayObject(id, position)
-    local displayObject = display.newImageRect(DisplayGroups.cards, "pics/" .. id .. ".jpg", self.width, self.height)
+    local displayObject = display.newGroup()
+    DisplayGroups.cards:insert(displayObject)
     self.displayObject = displayObject
+
+    local image = display.newImageRect(displayObject, "pics/" .. id .. ".jpg", self.width, self.height)
+    self.image = image
+
     local finalX = self:_getXAtPosition(position)
     local finalY = self:_getYAtPosition(self.side, position)
     displayObject.x = finalX + display.contentWidth * 1.5
@@ -47,8 +53,8 @@ function CardView:createDisplayObject(id, position)
     --fusion material number
     local materialNumber = {}
     materialNumber.displayGroup = display.newGroup()
-    materialNumber.displayGroup.x = finalX - displayObject.width / 2 + 22
-    materialNumber.displayGroup.y = displayObject.y - displayObject.height / 2
+    materialNumber.displayGroup.x = finalX - image.width / 2 + 22
+    materialNumber.displayGroup.y = displayObject.y - image.height / 2
     DisplayGroups.ui:insert(materialNumber.displayGroup)
 
     materialNumber.rect = display.newRoundedRect(materialNumber.displayGroup, 0, 0, 50, 30, 5)
@@ -61,7 +67,10 @@ function CardView:createDisplayObject(id, position)
     self.materialNumber = materialNumber
     self:_hideMaterialNumber()
 
-    displayObject:addEventListener("touch", self)
+    image:addEventListener("touch", self)
+
+    self.text = CardText:new(self.model)
+    displayObject:insert(self.text.displayObject)
 end
 
 function CardView:touch(event)
@@ -127,14 +136,14 @@ function CardView:getCardRotationPath(angle)
 end
 
 function CardView:setCardRotation(angle)
-    local path = self.displayObject.path
+    local path = self.image.path
     for p, v in pairs(self:getCardRotationPath(angle)) do
         path[p] = v
     end
 end
 
 function CardView:rotateCardTo(params)
-    local path = self.displayObject.path
+    local path = self.image.path
     for p, v in pairs(self:getCardRotationPath(params.angle)) do
         params[p] = v
     end
@@ -153,7 +162,7 @@ function CardView:summon(position)
             time = 400,
             transition = easing.inOutSine,
             x = self:_getXAtPosition(position),
-            y = self.side == 1 and self.displayObject.height / 2 or -self.displayObject.height / 2
+            y = self.side == 1 and self.image.height / 2 or -self.image.height / 2
         },
         true
     )
@@ -175,11 +184,11 @@ function CardView:_showMaterialNumber()
 end
 
 function CardView:_getXAtPosition(position)
-    return display.contentCenterX + (position - 3) * self.displayObject.width
+    return display.contentCenterX + (position - 3) * self.image.width
 end
 
 function CardView:_getYAtPosition(position)
-    return display.contentHeight - (self.side == 1 and 0.5 or 3.5) * self.displayObject.height
+    return display.contentHeight - (self.side == 1 and 0.5 or 3.5) * self.image.height
 end
 
 return CardView
